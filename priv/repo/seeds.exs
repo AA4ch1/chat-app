36dc5e09d925c62b0038340f436b19190b1070c3
AA4ch1 <aa4ch1@gmail.com>
2017-05-17 19:10:54 +0900

    Add seeds.exs and faker to dependency

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,18 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule ChatApp.DatabaseSeeder do
+  alias ChatApp.Repo
+  alias ChatApp.Message
+
+  def insert_message do
+    Repo.insert! %Message{
+      content: Faker.Lorem.paragraph(%Range{first: 1, last: 2}),
+      chat_room_id: 1
+    }
+  end
+end
+
+(1..10)
+|> Enum.each(fn _ -> ChatApp.DatabaseSeeder.insert_message end)
