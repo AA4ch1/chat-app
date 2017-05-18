efbcfaf21e6cb5e82326e692f0d88eae675cc779
AA4ch1 <aa4ch1@gmail.com>
2017-05-18 15:14:01 +0900

    Add exception to api routing

--- web/router.ex
+++ web/router.ex
@@ -26,7 +26,7 @@ defmodule ChatApp.Router do
     pipe_through :api
 
     scope "/v1", V1, as: :v1 do
-      resources "/chat_rooms", ChatRoomController do
+      resources "/chat_rooms", ChatRoomController, except: [:new, :edit] do
         get "/messages", ChatRoomController, :index_message, as: :index_message
         post "/messages", ChatRoomController, :create_message, as: :create_message
         delete "/messages/:id", ChatRoomController, :delete_message, as: :delete_message
